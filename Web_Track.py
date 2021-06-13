c50da83f004293ae87b3ecfc29e650fb36cb02d6
rahul3613 <53302959+rahul3613@users.noreply.github.com>
2021-06-13 16:11:56 +0530

    Update Web_Track.py

--- Web_Track.py
+++ Web_Track.py
@@ -14,7 +14,7 @@ while True:
     print('New loop Time: ' + str(datetime.now().strftime("%H:%M:%S")))
     
     try:
-        response = requests.get("https://slotalert.herokuapp.com/slot/getdata", timeout=30)
+        response = requests.get("https://slotalert.herokuapp.com/slot/getfulldata", timeout=30)
         web_data = response.json()
 
         for day in [1,2,3,1,4,5]:
@@ -44,17 +44,18 @@ while True:
 
                             if str(cent['pincode']) == str(pinc):
                                 for user in web_data[distt][pinc].keys():
+                                    if web_data[distt][pinc][user]['notify']=='True':
 
-                                    if (web_data[distt][pinc][user]['d1']=='True' and cent['available_capacity_dose1'] > 0) or (web_data[distt][pinc][user]['d2']=='True' and cent['available_capacity_dose2'] > 0):
-                                        if (web_data[distt][pinc][user]['eight']=='True' and cent['min_age_limit']== 18) or (web_data[distt][pinc][user]['five']=='True' and cent['min_age_limit']==45):
+                                        if (web_data[distt][pinc][user]['d1']=='True' and cent['available_capacity_dose1'] > 0) or (web_data[distt][pinc][user]['d2']=='True' and cent['available_capacity_dose2'] > 0):
+                                            if (web_data[distt][pinc][user]['eight']=='True' and cent['min_age_limit']== 18) or (web_data[distt][pinc][user]['five']=='True' and cent['min_age_limit']==45):
 
-                                            if (web_data[distt][pinc][user]['free']=='True' and cent['fee_type']=='Free') or (web_data[distt][pinc][user]['paid']=='True' and cent['fee_type']=='Paid'):
-                                                if (web_data[distt][pinc][user]['cvsh']=='True' and cent['vaccine']=='COVISHIELD') or (web_data[distt][pinc][user]['covax']=='True' and cent['vaccine']=='COVAXIN') or (web_data[distt][pinc][user]['sptk']=='True' and cent['vaccine']=='SPUTNIK V'):
-                                                    if cent['center_id'] not in web_data[distt][pinc][user]['slot'][str(day)]:
+                                                if (web_data[distt][pinc][user]['free']=='True' and cent['fee_type']=='Free') or (web_data[distt][pinc][user]['paid']=='True' and cent['fee_type']=='Paid'):
+                                                    if (web_data[distt][pinc][user]['cvsh']=='True' and cent['vaccine']=='COVISHIELD') or (web_data[distt][pinc][user]['covax']=='True' and cent['vaccine']=='COVAXIN') or (web_data[distt][pinc][user]['sptk']=='True' and cent['vaccine']=='SPUTNIK V'):
+                                                        if cent['center_id'] not in web_data[distt][pinc][user]['slot'][str(day)]:
 
-                                                        put = requests.get('https://slotalert.herokuapp.com/slot/putdata/{}/{}/{}/'.format(user, day, cent['center_id']))
-                                                        print('Updated: /putdata/{}/{}/{}/'.format(user, day, cent['center_id']))
-                                                        print('Pin: ' + str(cent['pincode']))
+                                                            put = requests.get('https://slotalert.herokuapp.com/slot/putdata/{}/{}/{}/'.format(user, day, cent['center_id']))
+                                                            print('Updated: /putdata/{}/{}/{}/'.format(user, day, cent['center_id']))
+                                                            print('Pin: ' + str(cent['pincode']))
 
     except:
         print("Sleeping due to error")
